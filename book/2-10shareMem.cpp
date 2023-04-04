e51e68801175defef42d2a5e6d9d9e08b6e2f10a
zhanghao <2573344091@qq.com>
2023-04-04 17:38:52 +0800

    回看2-10共享内存

--- book/2-10shareMem.cpp
+++ book/2-10shareMem.cpp
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+//共享内存用这个结构体控制。
 struct st_pid{
     int pid; //进程编号
     char name[51]; //进程名称
@@ -17,7 +17,7 @@ int main(int argc,char * argv[]){
     //共享内存的标志
     int shmid;
 
-    //shmget 获取或者创建共享内存，剪枝为0x5005
+    //shmget 获取或者创建共享内存，key键值设为0x5005
     if((shmid=shmget(0x5005,sizeof(struct st_pid),0640|IPC_CREAT)) == -1)//0640是权限
     {
         printf("shmget(0x5005) failed \n");return -1;
@@ -25,7 +25,7 @@ int main(int argc,char * argv[]){
     //用于指向共享内存的结构体变量
     struct st_pid * stpid = 0;
 
-    //把共享内存连接到当前进程的地址空间
+    //把共享内存连接到当前进程的地址空间// shmat(attach)
     if ((stpid = (struct st_pid * )shmat(shmid,0,0)) == (void *)-1){
         printf("shmat failed\n");
         return -1;
@@ -35,9 +35,8 @@ int main(int argc,char * argv[]){
     stpid->pid = getpid();
     strcpy(stpid->name,argv[1]);
     printf("pid = %d,name = %s\n",stpid->pid,stpid->name);
-    //把共享内存从当前进程中分离。
+    //把共享内存从当前进程中分离。//shmdt(dettach)
     shmdt(stpid);
-
 
     //删除共享内存
     if(shmctl(shmid,IPC_RMID,0) == -1){
